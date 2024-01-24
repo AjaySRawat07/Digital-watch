221e6f61b687a206208faa17c0535373afe0a149
AjaySRawat07 <ajju246455@gmail.com>
2024-01-25 00:38:55 +0530

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+let hrs = document.querySelector("#hrs");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+let format = document.querySelector("#format");
+let ampm = document.querySelector("#ampm");
+setInterval(()=>{
+    let currentTime = new Date();
+// console.log(currentTime);
+if(format.value == 24){
+    ampm.innerHTML= "";
+}
+else{
+    ampm.innerHTML = hrs>12? "pm": "am"; 
+}
+hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
+sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
+},1000);
+
